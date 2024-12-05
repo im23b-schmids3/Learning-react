4c77ad8c80818dc14806b5dccf4f73c3cf867af2
schmids3 <schmids3@bzz.ch>
2024-12-05 13:49:24 +0100

    Introduction Tailwind

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,14 @@
+import Product from '../components/Product';
+export default function ProductList() {
+    return (
+        <div className='flex flex-wrap w-full p-5'>
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    )
+}
