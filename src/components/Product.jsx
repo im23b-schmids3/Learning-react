d452fc1611227b0484cfa26a7c5cd3f8790d541b
schmids3 <schmids3@bzz.ch>
2024-12-05 15:37:49 +0100

    Add more pages

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,11 @@
 export default function Product(props) {
     const product = props.product;
     return (
-        <div className='w-1/4 p-5 border-2 border-gray-300'>
+        <div className='w-1/4 p-5 border border-gray-300'>
             <img src={product.thumbnail} alt={product.name}/>
             <h2 className="font-bold">{product.title}</h2>
-            <p>{product.description}</p>
-            <p className="text-gray-500">{product.price}</p>
+            <p className="text-gray-700">{product.description}</p>
+            <p className="text-black">{product.price}</p>
         </div>
     )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,22 +2,31 @@ import Product from '../components/Product';
 import {useEffect, useState} from "react";
 
 export default function ProductList() {
+    const limit = 12;
+    const [page, setPage] = useState(1);
     const [products, setProducts] = useState([]);
+    let skip = limit * (page - 1);
 
     useEffect(() => {
-        fetch("https://dummyjson.com/products")
+
+        fetch(`https://dummyjson.com/products?limit=${limit}&skip=${skip}`)
             .then(res => res.json())
             .then(data => {
                 setProducts(data.products);
             })
         // Fetch products from API
         // Set products state with fetched data
-    }, []);
+    }, [page]);
     return (
         <div className='flex flex-wrap w-full p-5'>
             {products.map((product) => (
                 <Product key={product.id} product={product}/>
             ))}
+
+            <button className="p-3" onClick={() => setPage(prevPage => (prevPage - 1))} disabled={page === 1}
+            > prev </button>
+            <p className="p-3 text-blue-700">{page}</p>
+            <button className="p-3" onClick={() => setPage(page + 1)}> next</button>
         </div>
     )
 }
